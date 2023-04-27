5ae504c39be8aae36a8330d1a9713ea6079d318a
chloetorch <125360014+chloetorch@users.noreply.github.com>
2023-04-27 16:49:26 +0300

    Add files via upload

---
+++ Задание 3/Задание 3.js
@@ -0,0 +1,20 @@
+const wsUri = 'wss://echo-ws-service.herokuapp.com';
+
+const sendButton = document.querySelector('btn-send'); // const button = document.querySelector('#sendMe');
+const geolocationButton = document.querySelector('btn-geo');
+
+let websocket;
+
+function writeToScreen(message) {
+   let pre = document.createElement("p");
+   pre.style.wordWrap = "break-word";
+   pre.innerHTML = message;
+   output.appendChild(pre);
+}
+
+sendButton.addEventListener('click', () => {
+   const message = 'Test message';
+   writeToScreen("SENT: " + message);
+   websocket.send(message);
+});
+
