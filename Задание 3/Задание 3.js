7f69217ecfb6c3841318acd51f0efb8cc7fe8892
chloetorch <125360014+chloetorch@users.noreply.github.com>
2023-04-28 11:54:00 +0300

    Update Задание 3.js

--- Задание 3/Задание 3.js
+++ Задание 3/Задание 3.js
@@ -6,32 +6,40 @@ const geolocationButton = document.querySelector('.btn-geo');
 let websocket;
 
 websocket = new WebSocket(wsUri);
-websocket.onopen = function(evt) {
-  // writeToScreen("CONNECTED");
-};
+websocket.onopen = function (evt) { };
 
 function writeToScreen(message) {
-  let pre = document.createElement("p");
-  pre.style.wordWrap = "break-word";
-  pre.innerHTML = message;
-  output.appendChild(pre);
-  }
+   let pre = document.createElement("p");
+   pre.style.wordWrap = "break-word";
+   pre.innerHTML = message;
+   output.appendChild(pre);
+}
 
 sendButton.addEventListener('click', () => {
-  const input = document.getElementById('input').value;
-  const message = input;
-  writeToScreen("Отправитель: " + message);
-  websocket.send(message);
-    
-    websocket.onmessage = function(evt) {
-    writeToScreen(
-      '<span">Сервер: ' + evt.data + '</span>'
-    );
-  };
-  websocket.onerror = function(evt) {
-    writeToScreen(
-      '<span style="color: red;">ERROR:</span> ' + evt.data
-    );
-  };
+   const input = document.getElementById('input').value;
+   const message = input;
+   writeToScreen("Отправитель: " + message);
+   websocket.send(message);
 
+   websocket.onmessage = function (evt) {
+      writeToScreen(
+         '<span>Сервер: ' + evt.data + '</span>'
+      );
+   };
+   websocket.onerror = function (evt) {
+      writeToScreen(
+         '<span style="color: red;">ERROR:</span> ' + evt.data
+      );
+   };
 });
+
+geolocationButton.addEventListener('click', () => {
+   let position = navigator.geolocation.getCurrentPosition((position) => {
+      writeToScreen(
+         `<a href="https://www.openstreetmap.org/#map=9/${position.coords.latitude}/${position.coords.longitude}">geolocation</a>`
+      );
+      websocket.send = function (position) { };
+   })
+})
+
+
