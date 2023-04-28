30fdcfd816416c6a5fa1f896f2adcf4ef1367781
chloetorch <125360014+chloetorch@users.noreply.github.com>
2023-04-28 11:05:38 +0300

    Update Задание 3.js

--- Задание 3/Задание 3.js
+++ Задание 3/Задание 3.js
@@ -1,37 +1,37 @@
 const wsUri = 'wss://echo-ws-service.herokuapp.com';
 
-const sendButton = document.querySelector('btn-send'); 
-const geolocationButton = document.querySelector('btn-geo');
+const sendButton = document.querySelector('.btn-send');
+const geolocationButton = document.querySelector('.btn-geo');
 
 let websocket;
 
+websocket = new WebSocket(wsUri);
+websocket.onopen = function(evt) {
+  writeToScreen("CONNECTED");
+};
+
 function writeToScreen(message) {
-   let pre = document.createElement("p");
-   pre.style.wordWrap = "break-word";
-   pre.innerHTML = message;
-   output.appendChild(pre);
-}
+  let pre = document.createElement("p");
+  pre.style.wordWrap = "break-word";
+  pre.innerHTML = message;
+  output.appendChild(pre);
+  }
 
 sendButton.addEventListener('click', () => {
-   websocket = new WebSocket(wsUri);
-   websocket.onopen = function (evt) {
-      writeToScreen("CONNECTED");
-   };
-
-   const input = document.getElementById('input').value;
-   const message = "Отправитель: " + input;
-   writeToScreen(message);
-   websocket.send(message);
-
-   websocket.onmessage = function (evt) {
-      writeToScreen(
-         '<span style="color: blue;">RESPONSE: ' + evt.data + '</span>'
-      );
-   };
-   websocket.onerror = function (evt) {
-      writeToScreen(
-         '<span style="color: red;">ERROR:</span> ' + evt.data
-      );
-   };
+  const input = document.getElementById('input').value;
+ const message = "Отправитель: " + input;
+  writeToScreen(message);
+  websocket.send(message);
+    
+    websocket.onmessage = function(evt) {
+    writeToScreen(
+      '<span style="color: blue;">RESPONSE: ' + evt.data+'</span>'
+    );
+  };
+  websocket.onerror = function(evt) {
+    writeToScreen(
+      '<span style="color: red;">ERROR:</span> ' + evt.data
+    );
+  };
 
 });
